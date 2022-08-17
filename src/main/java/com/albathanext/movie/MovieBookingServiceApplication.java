47d22f4cd4b75f1d3176a02ed9c56c45047cca0d
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-08-18 01:11:13 +0400

    disable caching for checking deployment

--- src/main/java/com/albathanext/movie/MovieBookingServiceApplication.java
+++ src/main/java/com/albathanext/movie/MovieBookingServiceApplication.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.cache.annotation.EnableCaching;
 
 
-@EnableCaching
+// @EnableCaching
 @AutoConfiguration
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Movie Booking API", version = "1.0", description = "provides APIs to find and book Movies"))
